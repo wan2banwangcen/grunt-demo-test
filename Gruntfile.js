f1a3518958e43318bf3ca37c0328b46536a976f3
wan2banwangcen <715586415@qq.com>
2019-03-12 16:54:58 +0800

    sprite

---
+++ Gruntfile.js
@@ -0,0 +1,13 @@
+module.exports = function(grunt){
+  grunt.initConfig({
+    sprite:{
+      all:{
+        src:'images/*.png',
+        dest:'dist/spritesheet.png',
+        destCss:'dist/sprites.css'
+      }
+    }
+  });
+  grunt.loadNpmTasks('grunt-spritesmith');
+  grunt.registerTask('default',['sprite']);
+}
