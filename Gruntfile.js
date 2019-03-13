86e5a3e785b6d7b8fb12004d973b17aed435fe8c
wan2banwangcen <715586415@qq.com>
2019-03-13 15:49:36 +0800

    http-api-test

---
+++ Gruntfile.js
@@ -0,0 +1,14 @@
+module.exports = function(grunt){
+  grunt.initConfig({
+    mochacli:{
+      options:{
+        reporter:'spec',
+        bail:true
+      },
+      all:['test/*.js']
+    }
+  });
+    
+    grunt.loadNpmTasks('grunt-mocha-cli');
+    grunt.registerTask('default', ['mochacli']);
+};
